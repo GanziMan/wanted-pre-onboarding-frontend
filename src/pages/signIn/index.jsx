e8bcf93f7ac94e74f424ae0eb9f204fa4655f526
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2023-06-15 01:22:16 +0900

    [EDIT] 로그인 id값 추가

--- src/pages/signIn/index.jsx
+++ src/pages/signIn/index.jsx
@@ -4,20 +4,20 @@ const SignIn = () => {
       <tr>
         <td>이메일</td>
         <td>
-          <input type="text"></input>
+          <input type="text" data-testid="email-input"></input>
         </td>
       </tr>
       <tr>
         <td>비밀번호</td>
         <td>
-          <input type="password" />
+          <input type="password" data-testid="password-input" />
         </td>
       </tr>
       <tr>
         <td></td>
         <td>
           <input type="submit" value="회원가입" />
-          <input type="submit" value="로그인" />
+          <input type="submit" data-testid="signin-button" value="로그인" />
         </td>
       </tr>
     </table>
