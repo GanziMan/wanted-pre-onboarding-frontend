8ff3a3792e94714a34a347ffcf2aeccf48742a5f
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2023-06-15 01:21:56 +0900

    [EDIT] 회원가입 틀 추가

--- src/pages/signUp/index.jsx
+++ src/pages/signUp/index.jsx
@@ -1,5 +1,26 @@
 const SignUp = () => {
-  return <div>I'm signUP</div>;
+  return (
+    <table>
+      <tr>
+        <td>이메일</td>
+        <td>
+          <input type="text" data-testid="email-input"></input>
+        </td>
+      </tr>
+      <tr>
+        <td>비밀번호</td>
+        <td>
+          <input type="password" data-testid="password-input" />
+        </td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>
+          <input type="submit" data-testid="signup-button" value="회원가입" />
+        </td>
+      </tr>
+    </table>
+  );
 };
 
 export default SignUp;
