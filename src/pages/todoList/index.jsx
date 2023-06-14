a1f60b4c6930b15f362a609dbba3c78b99fedb78
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2023-06-15 08:56:02 +0900

    [EDIT] TodoList 틀 작업

--- src/pages/todoList/index.jsx
+++ src/pages/todoList/index.jsx
@@ -1,5 +1,33 @@
+import { useEffect } from "react";
+import useTodo from "../../hooks/useTodo";
+
 const TodoList = () => {
-  return <div>I'm TodoList</div>;
+  const { getTodos } = useTodo();
+  useEffect(() => {
+    getTodos();
+  }, []);
+  return (
+    <>
+      <input data-testid="new-todo-input" />
+      <button data-testid="new-todo-add-button">추가</button>
+      <li>
+        <label>
+          <input type="checkbox" />
+          <span>TODO 1</span>
+          <button data-testid="modify-button">수정</button>
+          <button data-testid="delete-button">삭제</button>
+        </label>
+      </li>
+      <li>
+        <label>
+          <input type="checkbox" />
+          <span>TODO 2</span>
+          <button data-testid="modify-button">수정</button>
+          <button data-testid="delete-button">삭제</button>
+        </label>
+      </li>
+    </>
+  );
 };
 
 export default TodoList;
